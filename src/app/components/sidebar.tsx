f1266a5d11bb80abb50aa2e14cdce8bb485b3890
MykhailoVobolis <mvobolis@gmail.com>
2024-08-27 00:11:39 +0300

    add navigation

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,10 +1,23 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
-import SidebarItem from '@/app/components/sidebar-item';
+import SidebarItem from './sidebar-item';
+import { usePathname, useRouter } from 'next/navigation';
 
 export interface SidebarProps {}
 
 export default function Sidebar({}: SidebarProps) {
+  // Отримання об'єкта router, який містить інформацію про поточний маршрут та надає методи для програмної навігації всередині додатку.
+  const router = useRouter();
+  // Визначення поточного маршруту url
+  const pathname = usePathname();
+
+  const handleExitClick = () => {
+    // Програмна навігація на новий url. Перенаправлює не сторінку '/'
+    router.push('/');
+  };
+
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
@@ -17,6 +30,7 @@ export default function Sidebar({}: SidebarProps) {
         />
         <ul className="space-y-7">
           <SidebarItem
+            current={pathname === '/dashboard'}
             pathname="/dashboard"
             src="/icons/squares.svg"
             alt="dashboard icon"
@@ -24,6 +38,7 @@ export default function Sidebar({}: SidebarProps) {
             Dashboard
           </SidebarItem>
           <SidebarItem
+            current={pathname === '/companies'}
             pathname="/companies"
             src="/icons/briefcase.svg"
             alt="companies icon"
@@ -31,7 +46,10 @@ export default function Sidebar({}: SidebarProps) {
             Companies
           </SidebarItem>
         </ul>
-        <button className="flex items-center gap-2 p-6 mt-auto mx-auto">
+        <button
+          className="flex items-center gap-2 p-6 mt-auto mx-auto"
+          onClick={handleExitClick}
+        >
           <Image
             width={18}
             height={18}
