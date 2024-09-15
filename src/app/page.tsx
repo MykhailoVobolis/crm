106bff05a89db026e5ec051a74d8fd4ed34f799d
MykhailoVobolis <mvobolis@gmail.com>
2024-09-15 17:43:04 +0300

    add link to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
+      <p>
+        Go to{' '}
+        <Link
+          href="/dashboard"
+          className="text-blue-700 no-underline hover:text-blue-300 hover:underline"
+        >
+          Dashboard
+        </Link>
+      </p>
     </main>
   );
 }
